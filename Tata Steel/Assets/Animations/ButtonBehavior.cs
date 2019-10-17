f01a722cef42a8ad2f958d84b4795422ff96091b
VRstudent <Mulderyannick@gmail.com>
2019-10-17 16:10:51 +0200

    Beautiful stuff that needs to be pushed

---
+++ Tata Steel/Assets/Animations/ButtonBehavior.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    Animator m_Animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+
+        m_Animator.SetBool("SlowCycle", false);
+        m_Animator.SetBool("FastCycle", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StopFan()
+    {
+        m_Animator.SetBool("SlowCycle", false);
+        m_Animator.SetBool("FastCycle", false);
+
+    }
+
+    public void Click1Speed()
+    {
+        m_Animator.SetBool("SlowCycle", true);
+        m_Animator.SetBool("FastCycle", false);
+    }
+    public void Click2Speed()
+    {
+        m_Animator.SetBool("FastCycle", true); 
+
+    }
+}

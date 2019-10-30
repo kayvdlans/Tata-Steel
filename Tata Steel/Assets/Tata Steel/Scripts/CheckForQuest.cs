4788e3c849c62864ab313043a1c9f8b513abe151
Yannick Mulder <Mulderyannick@gmail.com>
2019-10-30 15:35:29 +0100

    Clipboard that works

---
+++ Tata Steel/Assets/Tata Steel/Scripts/CheckForQuest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckForQuest : MonoBehaviour
+{
+    public Quest quest;
+
+    public Text objective;
+    public Text discription;
+    public Text score;
+    public GameObject pauseMenu;
+    int reward = 0;
+
+    private void Start()
+    {
+    }
+
+    public void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            OpenMenu();
+        }
+
+        score.text = reward.ToString() + " /500";
+    }
+
+    public void OpenMenu()
+    {
+        quest.isActive = true;
+        pauseMenu.SetActive(true);
+        objective.text = quest.objective;
+        discription.text = quest.discription;
+    }
+
+    public void CloseMenu()
+    {
+        pauseMenu.SetActive(false);
+        quest.goal.Worked();
+        if (quest.goal.IsReached())
+        {
+            reward = quest.reward;
+            quest.isActive = false;
+        }
+    }
+
+}
---
+++ Tata Steel/Assets/Tata Steel/Scripts/Objectives.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Objectives
+{
+    public bool isActive;
+
+    public string objective;
+    public string progress;
+    public int reward;
+
+    public int requiredAmount;
+    public int currentAmount;
+
+    public bool IsReached()
+    {
+        return (currentAmount >= requiredAmount);
+        
+    }   
+
+
+    public ObjectGoal goal;
+}

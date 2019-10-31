3d0316a61102b26dce5c091422ca511b50644432
VRstudent <Mulderyannick@gmail.com>
2019-10-31 16:52:42 +0100

    someupdatessomefixes

---
+++ Tata Steel/Assets/Tata Steel/Scripts/Interaction/RemoveOnCollision.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveOnCollision : MonoBehaviour
+{
+    [SerializeField] private Collider colliderObject;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.Equals(colliderObject))
+        {
+            Destroy(gameObject);
+        }
+    }
+}

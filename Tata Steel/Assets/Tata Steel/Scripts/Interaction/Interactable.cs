fb0fa8980a7a2225de76db05b60aeb90f82461d6
kayvdlans <kayvanderlans@gmail.com>
2019-11-04 10:31:12 +0100

    Reverted some changes as it was creating more problems than it did good

--- Tata Steel/Assets/Tata Steel/Scripts/Interaction/Interactable.cs
+++ Tata Steel/Assets/Tata Steel/Scripts/Interaction/Interactable.cs
@@ -21,18 +21,8 @@ public class Interactable : MonoBehaviour
     [SerializeField] private UnityEvent onEndInteraction;
     [SerializeField] private bool continueOutOfRange;
 
-    protected MeshRenderer[] highlightRenderers;
-    protected MeshRenderer[] existingRenderers;
-    protected GameObject highlightHolder;
-    protected SkinnedMeshRenderer[] highlightSkinnedRenderers;
-    protected SkinnedMeshRenderer[] existingSkinnedRenderers;
-    //protected static Material highlightMat;
-    [Tooltip("An array of child gameObjects to not render a highlight for. Things like transparent parts, vfx, etc.")]
-    public GameObject[] hideHighlight;
-
     //[SerializeField] private Material material;
     [SerializeField] private Material outlineMaterial;
-    [SerializeField] private bool useNewOutlineRenderer = false;
 
     [SerializeField] private List<Renderer> renderers;
 
@@ -154,12 +144,6 @@ public class Interactable : MonoBehaviour
         return OVRInput.RawButton.None;
     }
 
-    private void Update()
-    {
-        if (useNewOutlineRenderer)
-        UpdateHighlightRenderers();
-    }
-
     private void FixedUpdate()
     {
         if (canInteract)
@@ -257,12 +241,6 @@ public class Interactable : MonoBehaviour
             if (!hands.Contains(other.transform))
                 hands.Add(other.transform);
 
-            if (useNewOutlineRenderer)
-            {
-                CreateHighlightRenderers();
-                UpdateHighlightRenderers();
-            }
-
             StopAllCoroutines();
             StartCoroutine(CheckForClosestHand(0.1f));
         }
@@ -277,129 +255,6 @@ public class Interactable : MonoBehaviour
 
             StopAllCoroutines();
             StartCoroutine(CheckForClosestHand(0.1f));
-        }
-    }
-
-    protected virtual bool ShouldIgnoreHighlight(Component component)
-    {
-        return ShouldIgnore(component.gameObject);
-    }
-
-    protected virtual bool ShouldIgnore(GameObject check)
-    {
-        for (int ignoreIndex = 0; ignoreIndex < hideHighlight.Length; ignoreIndex++)
-        {
-            if (check == hideHighlight[ignoreIndex])
-                return true;
-        }
-
-        return false;
-    }
-
-    protected virtual void CreateHighlightRenderers()
-    {
-        existingSkinnedRenderers = this.GetComponentsInChildren<SkinnedMeshRenderer>(true);
-        highlightHolder = new GameObject("Highlighter");
-        highlightSkinnedRenderers = new SkinnedMeshRenderer[existingSkinnedRenderers.Length];
-
-        for (int skinnedIndex = 0; skinnedIndex < existingSkinnedRenderers.Length; skinnedIndex++)
-        {
-            SkinnedMeshRenderer existingSkinned = existingSkinnedRenderers[skinnedIndex];
-
-            if (ShouldIgnoreHighlight(existingSkinned))
-                continue;
-
-            GameObject newSkinnedHolder = new GameObject("SkinnedHolder");
-            newSkinnedHolder.transform.parent = highlightHolder.transform;
-            SkinnedMeshRenderer newSkinned = newSkinnedHolder.AddComponent<SkinnedMeshRenderer>();
-            Material[] materials = new Material[existingSkinned.sharedMaterials.Length];
-            for (int materialIndex = 0; materialIndex < materials.Length; materialIndex++)
-            {
-                materials[materialIndex] = outlineMaterial;
-            }
-
-            newSkinned.sharedMaterials = materials;
-            newSkinned.sharedMesh = existingSkinned.sharedMesh;
-            newSkinned.rootBone = existingSkinned.rootBone;
-            newSkinned.updateWhenOffscreen = existingSkinned.updateWhenOffscreen;
-            newSkinned.bones = existingSkinned.bones;
-
-            highlightSkinnedRenderers[skinnedIndex] = newSkinned;
-        }
-
-        MeshFilter[] existingFilters = this.GetComponentsInChildren<MeshFilter>(true);
-        existingRenderers = new MeshRenderer[existingFilters.Length];
-        highlightRenderers = new MeshRenderer[existingFilters.Length];
-
-        for (int filterIndex = 0; filterIndex < existingFilters.Length; filterIndex++)
-        {
-            MeshFilter existingFilter = existingFilters[filterIndex];
-            MeshRenderer existingRenderer = existingFilter.GetComponent<MeshRenderer>();
-
-            if (existingFilter == null || existingRenderer == null || ShouldIgnoreHighlight(existingFilter))
-                continue;
-
-            GameObject newFilterHolder = new GameObject("FilterHolder");
-            newFilterHolder.transform.parent = highlightHolder.transform;
-            MeshFilter newFilter = newFilterHolder.AddComponent<MeshFilter>();
-            newFilter.sharedMesh = existingFilter.sharedMesh;
-            MeshRenderer newRenderer = newFilterHolder.AddComponent<MeshRenderer>();
-
-            Material[] materials = new Material[existingRenderer.sharedMaterials.Length];
-            for (int materialIndex = 0; materialIndex < materials.Length; materialIndex++)
-            {
-                materials[materialIndex] = outlineMaterial;
-            }
-            newRenderer.sharedMaterials = materials;
-
-            highlightRenderers[filterIndex] = newRenderer;
-            existingRenderers[filterIndex] = existingRenderer;
-        }
-    }
-
-    protected virtual void UpdateHighlightRenderers()
-    {
-        if (highlightHolder == null)
-            return;
-
-        for (int skinnedIndex = 0; skinnedIndex < existingSkinnedRenderers.Length; skinnedIndex++)
-        {
-            SkinnedMeshRenderer existingSkinned = existingSkinnedRenderers[skinnedIndex];
-            SkinnedMeshRenderer highlightSkinned = highlightSkinnedRenderers[skinnedIndex];
-
-            if (existingSkinned != null && highlightSkinned != null && !isInteracting)
-            {
-                highlightSkinned.transform.position = existingSkinned.transform.position;
-                highlightSkinned.transform.rotation = existingSkinned.transform.rotation;
-                highlightSkinned.transform.localScale = existingSkinned.transform.lossyScale;
-                highlightSkinned.localBounds = existingSkinned.localBounds;
-                highlightSkinned.enabled = closestHand && existingSkinned.enabled && existingSkinned.gameObject.activeInHierarchy;
-
-                int blendShapeCount = existingSkinned.sharedMesh.blendShapeCount;
-                for (int blendShapeIndex = 0; blendShapeIndex < blendShapeCount; blendShapeIndex++)
-                {
-                    highlightSkinned.SetBlendShapeWeight(blendShapeIndex, existingSkinned.GetBlendShapeWeight(blendShapeIndex));
-                }
-            }
-            else if (highlightSkinned != null)
-                highlightSkinned.enabled = false;
-
-        }
-
-        for (int rendererIndex = 0; rendererIndex < highlightRenderers.Length; rendererIndex++)
-        {
-            MeshRenderer existingRenderer = existingRenderers[rendererIndex];
-            MeshRenderer highlightRenderer = highlightRenderers[rendererIndex];
-
-            if (existingRenderer != null && highlightRenderer != null && isInteracting == false)
-            {
-                highlightRenderer.transform.position = existingRenderer.transform.position;
-                highlightRenderer.transform.rotation = existingRenderer.transform.rotation;
-                highlightRenderer.transform.localScale = existingRenderer.transform.lossyScale;
-                highlightRenderer.enabled = closestHand && existingRenderer.enabled && existingRenderer.gameObject.activeInHierarchy;
-            }
-            else if (highlightRenderer != null)
-                highlightRenderer.enabled = false;
         }
     }
 }

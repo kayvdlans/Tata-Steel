d235178a656038146004670381fb09dc49ad737c
kayvdlans <kayvanderlans@gmail.com>
2019-11-07 12:01:31 +0100

    Can assign hands now

---
+++ Tata Steel/Assets/Tata Steel/Scripts/Interaction/AssignHands.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignHands : MonoBehaviour
+{
+    [SerializeField] Transform localAvatar;
+
+    Transform leftHand = null;
+    Transform rightHand = null;
+
+    private void Start()
+    {
+        StartCoroutine(Initialize(0.5f));
+    }
+
+    private IEnumerator Initialize(float waitTime)
+    {
+        while (!leftHand || !rightHand)
+        {
+            yield return new WaitForSeconds(waitTime);
+
+            leftHand = localAvatar.Find("hand_left");
+            rightHand = localAvatar.Find("hand_right");
+        }
+
+        leftHand.gameObject.AddComponent<Hand>();
+        rightHand.gameObject.AddComponent<Hand>();
+    }
+}

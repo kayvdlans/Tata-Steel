592980f3cce5801dbc4a3fb1637212c85fff8da8
VRstudent <Mulderyannick@gmail.com>
2019-11-08 12:00:43 +0100

    pusj

---
+++ Tata Steel/Assets/Tata Steel/Scripts/Interaction/InteractionInstantRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionInstantRotation : Interaction
+{
+    [SerializeField] [Range (1, 180)]
+    private float rotationAmount = 0;
+
+    protected override void Initialize()
+    {
+    }
+
+    protected override void OnInteractionStart()
+    {
+        base.OnInteractionStart();
+
+        Debug.Log(Vector3.SignedAngle(initialAttachPoint.position, transform.position, Vector3.right));
+
+        //if ()
+    }
+}

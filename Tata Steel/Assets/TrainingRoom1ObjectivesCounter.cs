b6446b805dc194693698ca3255106bc2a8da7728
Yannick <Mulderyannick@gmail.com>
2019-11-18 09:42:23 +0100

    VisualChecker

---
+++ Tata Steel/Assets/TrainingRoom1ObjectivesCounter.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainingRoom1ObjectivesCounter : MonoBehaviour
+{
+    public GameObject flask;
+    public GameObject stoomafsluiters;
+    public GameObject aftapsluiter;
+    // Update is called once per frame
+    private void Start()
+    {
+        flask.tag = "LookAt";
+        stoomafsluiters.tag = "LookAt";
+        aftapsluiter.tag = "LookAt";
+    }
+
+    void Update()
+    {
+
+    }
+}
+
---
+++ Tata Steel/Assets/VisualCheckChecker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VisualCheckChecker : MonoBehaviour
+{
+    private Transform _selection;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] private string ToLookAt ;
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_selection != null)
+          {
+            var selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.height / 2, Screen.width / 2));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selection = hit.transform;
+            if (selection.CompareTag(ToLookAt))
+            {
+                var selectionRenderer = _selection.GetComponent<Renderer>();
+                if (selectionRenderer != null)
+                {
+
+                }
+            }
+            _selection = selection;
+        }
+    }
+}

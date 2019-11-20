0fbf3f6225f14acc6309c4f1a64cd499100ddd4a
kaikon33 <kaikon33@users.noreply.github.com>
2019-11-20 15:14:10 +0100

    Machine room update

--- Tata Steel/Assets/Tata Steel/Scripts/Interaction/FlipSwitch.cs
+++ Tata Steel/Assets/Tata Steel/Scripts/Interaction/FlipSwitch.cs
@@ -20,23 +20,37 @@ public class FlipSwitch : MonoBehaviour
     private void Start()
     {
         originalEulers = s.localEulerAngles;
-        UpdateValue(startOn); 
+        UpdateValue(startOn);
     }
 
     public void Flip()
     {
         UpdateValue(!CurrentlyTurnedOn);
+        if (!CurrentlyTurnedOn)
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.pitch = Random.Range(0.8f, 1.5f);
+            audioSource.Play();
+        }
+        if(CurrentlyTurnedOn)
+        {
+            AudioSource audioSource = GetComponent<AudioSource>();
+            audioSource.pitch = Random.Range(0.8f, 1.5f);
+            audioSource.Pause();
+        }
+
     }
 
     private void UpdateValue(bool on)
     {
         CurrentlyTurnedOn = on;
 
-        currentAngle = on ? offOnRotation.y : offOnRotation.x;
+    
+    currentAngle = on? offOnRotation.y : offOnRotation.x;
 
-        s.localEulerAngles = new Vector3(
-            (int)axis == 0 ? currentAngle : originalEulers.x,
-            (int)axis == 1 ? currentAngle : originalEulers.y,
-            (int)axis == 2 ? currentAngle : originalEulers.z);
-    }
+    s.localEulerAngles = new Vector3(
+        (int) axis == 0 ? currentAngle : originalEulers.x,
+        (int) axis == 1 ? currentAngle : originalEulers.y,
+        (int) axis == 2 ? currentAngle : originalEulers.z);
+}
 }

4a735a239e81387654d4a5ef1043ef6822c40269
VRstudent <Mulderyannick@gmail.com>
2019-11-22 17:17:05 +0100

    UPstairs added

---
+++ Tata Steel/Assets/Downstairs.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Downstairs : MonoBehaviour
+{
+    GameObject player;
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+    public void TeleportToNewVector()
+    {
+        CharacterController c = player.GetComponent<CharacterController>();
+        c.enabled = false;
+        player.transform.position = new Vector3(4.278f, 0.435f, 0.36f);
+        c.enabled = true;
+    }
+}

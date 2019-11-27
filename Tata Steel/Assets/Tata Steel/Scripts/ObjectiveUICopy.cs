ade531f1c3fdcc8931de14addd1a531fe60d6feb
kayvdlans <kayvanderlans@gmail.com>
2019-11-27 10:28:04 +0100

    Added a copy of the objective UI for multiple screens.

---
+++ Tata Steel/Assets/Tata Steel/Scripts/ObjectiveUICopy.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectiveUICopy : MonoBehaviour
+{
+    [SerializeField] private Canvas parentCanvas;
+    [SerializeField] private ObjectiveDispencer oD;
+
+    private List<Text> objective = new List<Text>();
+    private List<Text> progress = new List<Text>();
+    private List<Text> done = new List<Text>();
+    private Text score = null;
+
+    private void Start()
+    {
+        StartCoroutine(WaitForInitialize());
+    }
+
+    private IEnumerator WaitForInitialize()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        objective = InitializeObjectsFromList(oD.objectiveText);
+        progress = InitializeObjectsFromList(oD.progressText);
+        done = InitializeObjectsFromList(oD.doneText);
+
+        score = Instantiate(oD.score, parentCanvas.transform);
+
+        oD.OnObjectiveDone += UpdateDoneText;
+    }
+
+    private List<Text> InitializeObjectsFromList(List<Text> list) 
+    {
+        List<Text> objects = new List<Text>();
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            objects.Add(Instantiate(list[i], parentCanvas.transform));
+        }
+
+        return objects;
+    }
+
+    private void UpdateDoneText(int index)
+    {
+        done[index].text = "Af: Ja";
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < objective.Count; i++)
+        {
+            if (progress.Count > 0)
+            {
+                progress[i].text = oD.objectives[i].currentAmount + " / " + oD.objectives[i].requiredAmount;
+            }
+        }
+
+        score.text = "behaalde score: " + oD.Points + "/" + oD.MaxPoints;
+    }
+}

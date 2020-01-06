36c0544b72da140865de4ea454ca7df7ff8a14db
VRstudent <Mulderyannick@gmail.com>
2020-01-06 15:12:37 +0100

    Auto stash before merge of "newinteractablesystem" and "origin/newinteractablesystem"

---
+++ Tata Steel/Assets/analytics.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class analytics : MonoBehaviour
+{
+    private void OnApplicationQuit()
+    {
+        Analytics.CustomEvent(Time.realtimeSinceStartup.ToString());
+    }
+}
